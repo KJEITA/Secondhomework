40b7f38d18ee1f954fcf224de0ba8a7972ef9c7c
Sergey <serega.putkin@yandex.ru>
2019-10-13 15:37:39 +0300

    final

--- app/src/main/java/bonch/dev/school/SecondActivity.kt
+++ app/src/main/java/bonch/dev/school/SecondActivity.kt
@@ -2,11 +2,22 @@ package bonch.dev.school
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var textView:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        textView = findViewById<TextView>(R.id.text_view)
+
+        var ind =  intent.getBooleanExtra ("Indicator",false)
+        var count =  intent.getIntExtra ("Counts",0)
+        var text =  intent.getStringExtra ("Text")
+
+        textView.text = "$ind\n$count\n$text"
     }
 }
