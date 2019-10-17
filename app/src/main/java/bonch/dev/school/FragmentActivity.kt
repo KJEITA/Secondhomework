9415d8e1e7823d9789763f2a50e506c41ed6f3c6
Sergey <serega.putkin@yandex.ru>
2019-10-17 13:26:44 +0300

    final_final

--- app/src/main/java/bonch/dev/school/FragmentActivity.kt
+++ app/src/main/java/bonch/dev/school/FragmentActivity.kt
@@ -11,29 +11,23 @@ import bonch.dev.school.fragments.SecondFragment
 class FragmentActivity : AppCompatActivity() {
 
     val fragmentManager = supportFragmentManager
-    private lateinit var button:Button
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
 
-        val firstFragment = FirstFragment()
-        fragmentManager.beginTransaction().add(R.id.fragment, firstFragment)
+        fragmentManager.beginTransaction().add(R.id.fragment, FirstFragment())
             .commit()
-
-            //button = findViewById<Button>(R.id.next_activity_button)
-
-        /*button.setOnClickListener(View.OnClickListener {
-            replaceFragment()
-        })
-    }*/
     }
-     fun replaceFragment(view :View){
-         //TODO Ты создал один фрагмент, а менеджеру присвоил новый
-         // (Если вдруг не очень хорошо с ООП — ты в менеджере вызвал конструктор и создал новый объект, а не передал предыдущий).
-         // Или передавай в менеджер secondActivity, или создавай фрагмент в менеджере, но убери неиспользуемый secondFragment
-        val secondFragment = SecondFragment()
-        fragmentManager.beginTransaction().add(R.id.fragment, SecondFragment()).addToBackStack("thirdFragment")
+
+    fun replaceFragment(view: View) {
+        //TODO Ты создал один фрагмент, а менеджеру присвоил новый
+        // (Если вдруг не очень хорошо с ООП — ты в менеджере вызвал конструктор и создал новый объект, а не передал предыдущий).
+        // Или передавай в менеджер secondActivity, или создавай фрагмент в менеджере, но убери неиспользуемый secondFragment
+
+        fragmentManager.beginTransaction().add(R.id.fragment, SecondFragment())
+            .addToBackStack("thirdFragment")
             .commit()
     }
 }
